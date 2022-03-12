2ffc19f242f046822e25b5d950816c5093d3b949
mythili annamalai <mythiliannamalai45@gmail.com>
2022-03-12 22:24:26 +0530

    Harmonic_number

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Harmonic_Number
+{
+    class Program
+    {
+        public void Calculation()
+        {
+            int i, n;
+            Console.WriteLine("enter the Nth value : ");
+            n=int.Parse(Console.ReadLine());
+            for(i=1;i<=n;i++)
+            {
+                Console.WriteLine(" 1/{0} +",i);
+            }
+        }
+    }
+    class Result
+    {
+        public static void Main(string[]args)
+        {
+            Program program = new Program();
+            program.Calculation();
+        }
+    }
+}
